fc9ef3fe49ba24cff1d7672fc9b87615799a6228
Tatiana <Rybalkinatr@gmail.com>
2023-02-21 13:13:46 +0400

    Решена задача 28

---
+++ Task28/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число N и выдает произведение от 1 до N
+int GetX(int N)
+{
+    int X = 1;
+    for(int i = 1; i <= N; i++)
+    X = X * i;
+    return X;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Произведение от 1 до {N} равна {GetX(N)}");
+
+
