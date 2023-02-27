123fc4c7e7c9ca6c6360f46b36daad6ee721919f
Tatiana <Rybalkinatr@gmail.com>
2023-02-27 13:38:37 +0400

    Task 35

---
+++ Task35/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте одномерный массив, состоящий из 123 случайных чисел. 
+//Найдите в нем количество чисел в отрезке от 10 до 99.
+
+int[] startArray = GetArray(123, 0 , 150);
+Console.WriteLine($"Количество элементов в отрезке [10;99] = {GetCountElements(startArray, 10, 99)}");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int GetCountElements(int[] array, int leftRange, int rigthRange)
+{
+    int count = 0;
+    foreach (var item in array)
+    {
+        if (item >= leftRange && item <= rigthRange) count++;
+    }
+    return count;
+}
+// Console.WriteLine("Введите число: ");
+// string number = Console.ReadLine();
+// int length = number.Length;
+
+// if (length == 5)
+// {
+//     if (number[0] == number[4] && number[1] == number[3])
+//     {
+//         Console.WriteLine($"{number} - Палиндром");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{number} - Не палиндром");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+// }
